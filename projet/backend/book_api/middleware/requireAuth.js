9581f587e417cb28fb21aca11538c7b6647aeb2e
rayenne11 <66142092+rayenne11@users.noreply.github.com>
2024-05-27 00:40:07 +0100

    vff

--- projet/backend/book_api/middleware/requireAuth.js
+++ projet/backend/book_api/middleware/requireAuth.js
@@ -30,28 +30,3 @@ const requireAuth = async (req, res, next) => {
 export default requireAuth;
 
 
-// import jwt from 'jsonwebtoken';
-
-// export function authMiddleware(req, res, next) {
-//   const token = req.header('Authorization').replace('Bearer ', '');
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Access denied. No token provided.' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ message: 'Invalid token.' });
-//   }
-// }
-
-// export function adminMiddleware(req, res, next) {
-//   if (req.user.username !== 'admin') {
-//     return res.status(403).json({ message: 'Access denied. Admins only.' });
-//   }
-//   next();
-// }
-
